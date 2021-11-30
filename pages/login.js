9e08843f653dbe5f747409f0a82379379fb7e083
jademon <j2python@gmail.com>
2021-11-30 17:44:21 +0900

    add login

---
+++ pages/login.js
@@ -0,0 +1,35 @@
+import Link from "next/Link";
+import Nav from '../components/nav'
+
+export default function Login() {
+  return (
+    <div className="w-full h-full bg-color">
+      <Nav />
+      <div className="flex-col mt-28 text-center">
+        <h2>로그인</h2>
+      </div>
+      <div className="flex-col mt-12">
+        <div className="text-center m-4">
+          <div><label htmlFor="id" className="inline-block w-60 text-start p-2 font-semibold">이메일</label></div>
+          <div><input type="text" id="id" className="w-60 border-b p-2 focus:outline-none" placeholder="이메일을 입력해주세요" required /></div>
+        </div>
+        <div className="text-center m-4">
+          <div><label htmlFor="id" className="inline-block w-60 text-start p-2 font-semibold">비밀번호</label></div>
+          <div><input type="password" id="pw" className="w-60 border-b p-2 focus:outline-none" placeholder="비밀번호를 입력해주세요" required /></div>
+        </div>
+        <div className="text-center m-4">
+          <div className="border rounded inline-block px-2 py-1 w-60 mt-8 hover-base-color hover:text-white base-color">
+            <Link href="/">
+              <button type="button" className="font-semibold">로그인</button>
+            </Link>
+          </div>
+        </div>
+        <div className="text-center m-4">
+          <div className="mt-10">
+            <Link href="/sign_up">회원가입</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
