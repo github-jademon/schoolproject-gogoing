52fc469bac6b1e0ba29cc4a8f4fc58d43910714f
jademon <j2python@gmail.com>
2021-12-01 15:56:42 +0900

    add check function

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,22 @@
 import Link from "next/Link";
 import Nav from '../components/nav'
+import { useState } from 'react'
 
 export default function Login() {
+  function check() {
+    var email = document.getElementById("email");
+    var pw = document.getElementById("pw");
+    if(email.value=="") {
+      alert("이메일을 입력해주세요");
+      email.focus();
+      return false;
+    }
+    else if(pw.value=="") {
+      alert("비밀번호를 입력해주세요");
+      pw.focus();
+      return false;
+    }
+  }
   return (
     <div className="w-full h-full bg-color">
       <Nav />
@@ -11,7 +26,7 @@ export default function Login() {
       <div className="flex-col mt-12">
         <div className="text-center m-4">
           <div><label htmlFor="id" className="inline-block w-60 text-start p-2 font-semibold">이메일</label></div>
-          <div><input type="text" id="id" className="w-60 border-b p-2 focus:outline-none" placeholder="이메일을 입력해주세요" required /></div>
+          <div><input type="text" id="email" className="w-60 border-b p-2 focus:outline-none" placeholder="이메일을 입력해주세요" required /></div>
         </div>
         <div className="text-center m-4">
           <div><label htmlFor="id" className="inline-block w-60 text-start p-2 font-semibold">비밀번호</label></div>
@@ -19,9 +34,7 @@ export default function Login() {
         </div>
         <div className="text-center m-4">
           <div className="border rounded inline-block px-2 py-1 w-60 mt-8 hover-base-color hover:text-white base-color">
-            <Link href="/">
-              <button type="button" className="font-semibold">로그인</button>
-            </Link>
+            <button type="button" className="font-semibold" onClick={(e) => check()}>로그인</button>  
           </div>
         </div>
         <div className="text-center m-4">
