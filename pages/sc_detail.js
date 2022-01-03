fe762c4d500cfe88fa20d0d9d1a5dfa267afd5d6
jademon <j2python@gmail.com>
2022-01-03 09:30:56 +0900

    modify div

--- pages/sc_detail.js
+++ pages/sc_detail.js
@@ -97,18 +97,17 @@ export default function scDetail() {
               <div className="mx-1">{data.data.sc_address}</div>
             </div>
           </div>
+          {/* <div id="map" style="width:340px;height:280px;" className="m-auto border my-4"></div>
           <div>
-            {/* <div id="map" style="width:340px;height:280px;" className="m-auto border my-4"></div> */}
-
-            {/* <Map
+            <Map
               center={{ lat: 33.5563, lng: 126.79581 }}
               style={{ width: "100%", height: "360px" }}
             >
               <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
                 <div style={{color:"#000"}}>Hello World!</div>
               </MapMarker>
-            </Map> */}
-          </div>
+            </Map>
+          </div> */}
         </div>
       </div>
     </>
